c9b9e032cad0d710d172430c99e6d6604858cc44
taikimushi <taikimushi@gmail.com>
2026-01-23 16:12:56 +0900

    add use formatDate

--- src/components/bloglist.jsx
+++ src/components/bloglist.jsx
@@ -1,9 +1,12 @@
+import { formatDate } from "../lib/utils";
+
 export default function BlogList({ blogs }) {
   return (
     <ul>
       {blogs.map(blog => (
         <li key={blog.id}>
           <h2>{blog.title}</h2>
+          <p>{formatDate(blog.publishedAt)}</p>
           <p>{blog.description}</p>
           <a href={`/blogs/${blog.id}`}>Read more</a>
         </li>
