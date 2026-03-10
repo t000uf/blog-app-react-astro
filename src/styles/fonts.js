22f3ea98837475847c3045bdc13e06b5b0cc3b3d
taikimushi <taikimushi@gmail.com>
2026-03-10 18:07:18 +0900

    Change: フォントのインポート方法変更

---
+++ src/styles/fonts.js
@@ -0,0 +1,5 @@
+import '@fontsource/zen-kaku-gothic-new/400.css';
+import '@fontsource/zen-kaku-gothic-new/500.css';
+import '@fontsource/zen-kaku-gothic-new/700.css';
+import '@fontsource/zen-kaku-gothic-new/900.css';
+import '@fontsource/zen-kurenaido/400.css';
