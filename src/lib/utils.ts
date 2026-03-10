0a637f0150680aef5fc7f4a077f71fdce7f61401
t000uf <83527784+taikimushi@users.noreply.github.com>
2026-03-10 16:27:43 +0900

    Revert "Feature/add linter"

--- src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-export const formatDate = (dateString: Date): string => {
-  const date = new Date(dateString);
-  return new Intl.DateTimeFormat('ja-JP', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  }).format(date);
-};
