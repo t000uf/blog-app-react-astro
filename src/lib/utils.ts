3eccf1a2d980b81ea5483904a5d85e6aeaf4a600
taikimushi <taikimushi@gmail.com>
2026-03-13 16:28:58 +0900

    Chore: clsxとtailwind-merge導入、cn関数追加とJSXに定義したtailwindがフォーマットされるようにした

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export const formatDate = (dateString: Date): string => {
   const date = new Date(dateString);
   return new Intl.DateTimeFormat('ja-JP', {
